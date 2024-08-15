591549575d46377083f1786aab3f903f84aab703
Ahamed <mohamed_marzook_ahamed_mujeeb_from.scientec@stb.gov.sg>
2024-08-15 18:14:51 +0800

    Back navigation after re-direction

--- src/app/components/openpage.ts
+++
@@ -1,3 +0,0 @@
-export default function directToPage(navigateUrl: String) {
-    window.open(navigateUrl.toString().trim(), '_blank');
-}
--- src/app/components/redirection.tsx
+++ src/app/components/redirection.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import IntermediateLoading from "./intermediateLoading";
+import { useRouter } from 'next/navigation';
 
 interface LaunchRedirectUrlProps {
     originalUrl: string;
@@ -9,11 +10,18 @@ interface LaunchRedirectUrlProps {
 
 export default function LaunchRedirectUrl(urlProps: LaunchRedirectUrlProps) {
     const [loading, setLoading] = useState<boolean>(true);
+    const router = useRouter();
+
     useEffect(() => {
         const timer = setTimeout(() => {
             window.open(urlProps.originalUrl, '_blank');
             setLoading(false);
-        }, 3000);
+            handleBackNavigation();
+        }, 5000);
+
+        const handleBackNavigation = () => {
+            router.back();
+        };
 
         return () => clearTimeout(timer); // Cleanup the timer on unmount
     }, []);
